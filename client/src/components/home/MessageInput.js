4a230bdf8d0d960035adb41b589fd34f31565704
Nicholas <najernigin@gmail.com>
2024-02-14 13:44:10 -0600

    Message socket

--- client/src/components/home/MessageInput.js
+++ client/src/components/home/MessageInput.js
@@ -3,43 +3,22 @@
 Form is structered using Formik */
 import React, { useState } from "react";
 
-function MessageInput({ url, room, messages, setMessages, user }) {
+function MessageInput({ user, socket, room }) {
   const [message, setMessage] = useState("");
 
-  function addMessage(e) {
+  const addMessage = (e) => {
     e.preventDefault();
 
-    fetch(`${url}/messages`, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({
-        message: message,
-        room_id: room.room_id,
-        user_id: user.user_id,
-      }),
-    })
-      .then((response) => {
-        if (response.ok) {
-          console.log("Message sent successfully");
-          return response.json();
-        } else {
-          console.error("Failed to send message");
-          throw new Error("Failed to send message");
-        }
-      })
-      .then((data) => {
-        const updatedMessages = Array.isArray(messages)
-          ? [...messages, data]
-          : [data];
-        setMessages(updatedMessages);
-        setMessage("");
-      })
-      .catch((error) => {
-        console.error("Error sending message:", error);
-      });
-  }
+    // Send message to WebSocket server
+    socket.emit("message", {
+      message,
+      room_id: room.room_id,
+      user_id: user.user_id,
+    });
+
+    // Reset message input
+    setMessage("");
+  };
 
   return (
     <form id="form-container" onSubmit={addMessage}>
