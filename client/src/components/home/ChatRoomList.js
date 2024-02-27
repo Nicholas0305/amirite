4e89be15046dacf79a428eddd048fbdab6eedce2
Nicholas <najernigin@gmail.com>
2024-02-27 12:04:41 -0600

    Message Bug Fix

--- client/src/components/home/ChatRoomList.js
+++ client/src/components/home/ChatRoomList.js
@@ -3,7 +3,7 @@
 import React from "react";
 import ChatRoomCard from "./ChatRoomCard";
 
-function ChatRoomList({ rooms, handlSelectRoom, user, deleteRoom }) {
+function ChatRoomList({ rooms, handleSelectRoom, user, deleteRoom }) {
   return (
     <ul id="public-rooms-list">
       {rooms.map((room) => {
@@ -11,7 +11,7 @@ function ChatRoomList({ rooms, handlSelectRoom, user, deleteRoom }) {
           <ChatRoomCard
             key={room.room_id}
             room={room}
-            handlSelectRoom={handlSelectRoom}
+            handleSelectRoom={handleSelectRoom}
             user={user}
             deleteRoom={deleteRoom}
             rooms={rooms}
